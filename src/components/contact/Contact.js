95d839436f32c6b4f324a289f744e83f272ba4de
unknown <amonmtz009@gmail.com>
2023-04-07 20:36:42 -0600

    adding emailjs dependency

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from './contact.module.css';
+import emailjs from 'emailjs-com';
 import contact from 'assets/img/contacto.jpg';
 import telephone from 'assets/img/phone-icon.jpg';
 import email from 'assets/img/email-icon.jpg';
 
 const Contact = () => {
+    const form = useRef();
+
+    const senEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID,
+            form.current,
+            process.env.REACT_APP_USER_ID
+        )
+        .then(
+            result => console.log(result.message),
+            error => console.log(error.text)
+        );
+
+        e.target.reset();
+    };
 
     return (
         <div className={styles['container-fluid']}>
@@ -31,15 +50,15 @@ const Contact = () => {
                     <div className={styles.card}>
                         <div className={styles['contact-card']}>
                             <h5 className={styles['card-title']}>Eviar mensaje</h5>
-                            <form>
-                                <label htmlFor="name">Nombre</label>
-                                <input type="text" name="name" placeholder="Escribe tu nombre" />
-                                <label htmlFor="email">Email</label>
-                                <input type="email" name="email" placeholder="Escribe tu email" />
+                            <form ref={form} onSubmit={senEmail}>
+                                <label htmlFor="user_name">Nombre</label>
+                                <input type="text" name="user_name" placeholder="Escribe tu nombre" />
+                                <label htmlFor="user_email">Email</label>
+                                <input type="email" name="user_email" placeholder="Escribe tu email" />
                                 <label htmlFor="asunto">Asunto</label>
                                 <input type="text" name="asunto" placeholder="Escribe el asunto" />
-                                <label htmlFor="mensaje">Mensaje</label>
-                                <textarea name="mensaje" cols="30" rows="10" placeholder="Dejanos tus dudas y/o comentarios" id={styles.message}></textarea>
+                                <label htmlFor="mensaje" required>Mensaje</label>
+                                <textarea name="message" cols="30" rows="10" placeholder="Dejanos tus dudas y/o comentarios" id={styles.message}></textarea>
                                 <button type="submit" id={styles.submit}>Enviar</button>
                             </form>
                         </div>
