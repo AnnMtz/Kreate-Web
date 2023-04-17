55720b7fb0d903e2d6ffe2ab079cb37088ba8d7d
unknown <amonmtz009@gmail.com>
2023-04-17 11:05:22 -0600

    commits deleted

--- src/exports/images.js
+++ src/exports/images.js
@@ -1,7 +1,5 @@
 import herramientas from 'assets/img/herramientas-crear-paginas-web.jpg';
-// import http from 'assets/img/https.jpg';
 import http from 'assets/img/www.jpg';
 import web from 'assets/img/webdeveloper.jpg';
-// import web from 'assets/img/desarrolloweb.jpg';
 
 export default [herramientas, http, web]
